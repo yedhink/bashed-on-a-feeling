b225437dc59c2992ef7cf5922775adbc03a441d5
yedhink <yedhin1998@gmail.com>
2018-05-26 18:00:09 +0530

    26 May 18 : Create/Update new files

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-cp ./.bashed-on-a-feeling.sh /home/${USER}/
-cat base.sh >> /home/${USER}/.bashrc
+cp ./.bashed-git-prompt.sh /home/${USER}/
+cat ./bashed-base.sh >> /home/${USER}/.bashrc
 source /home/${USER}/.bashrc
