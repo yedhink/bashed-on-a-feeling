4a77fef11c169150461e43161580f8b25e25d6f9
yedhink <yedhin1998@gmail.com>
2018-05-26 20:23:08 +0530

    26 May 18 : Create/Update new files

--- .bashed-gitprompt.sh
+++ .bashed-gitprompt.sh
@@ -17,8 +17,8 @@
 #  "ahh..ahh..ah..bashed-on-a-feeling!"
 #
 
-: ${commits_total:=''} # or you can try f737 with a nerd font
-: ${added_but_not_pushed:=''}
+: ${commiticon:=''} # or you can try f737 with a nerd font
+: ${added_but_not_committed:=''}
 : ${committed_but_not_pushed:=''}
 : ${no_of_files_to_be_pushed:=''}
 : ${committed_but_modified_before_push:=''}
@@ -40,13 +40,11 @@ while read -r Z; do
 	[[ "$Z" == commit* ]] && cno+=1
 done <<< "$(/usr/bin/git log 2> /dev/null)"
 commitstot=$cno
-commiticon="\\uf737"
-commiticon=`printf "%b\\n" "$commiticon"`
 
 if [ $a_but_not_c -eq 0 ];then
 	a_but_not_c=""
 else
-	a_but_not_c="$(tput bold)$(tput setaf 7)$a_but_not_c$(tput bold)$(tput setaf 2)"
+	a_but_not_c="$(tput bold)$(tput setaf 7)$a_but_not_c$(tput bold)$(tput setaf 2)$(echo $added_but_not_committed)"
 fi
 
 if [ $c_but_not_p -gt 0 ];then
@@ -54,21 +52,21 @@ if [ $c_but_not_p -gt 0 ];then
 fi
 
 if [ $c_but_not_p == 0 ];then
-	c_but_not_p="$(tput bold)$(tput setaf 2)"
+	c_but_not_p="$(tput bold)$(tput setaf 2)$(echo $committed_but_not_pushed)"
 else
-	c_but_not_p="$(tput bold)$(tput setaf 7)$c_but_not_p$(tput bold)$(tput setaf 2)$(echo $committed_but_not_pushed)"
+	c_but_not_p="$(tput bold)$(tput setaf 7)$c_but_not_p$(tput bold)$(tput setaf 2)$(echo $no_of_files_to_be_pushed)"
 fi
 
 if [ $c_but_m_before_p -eq 0 ];then
 	c_but_m_before_p=""
 else
-	c_but_m_before_p="$(tput bold)$(tput setaf 7)$c_but_m_before_p$(tput bold)$(tput setaf 2)"
+	c_but_m_before_p="$(tput bold)$(tput setaf 7)$c_but_m_before_p$(tput bold)$(tput setaf 2)$(echo $committed_but_modified_before_push)"
 fi
 
 if [ $untracked -eq 0 ];then
 	untracked=""
 else
-	untracked="$(tput bold)$(tput setaf 7)$untracked$(tput bold)$(tput setaf 2)"
+	untracked="$(tput bold)$(tput setaf 7)$untracked$(tput bold)$(tput setaf 2)$(echo $untracked_files)"
 fi
 # Create a string
 printf -v PS1RHS "\e[0m \e[0;1;31m%s %s %s %s %s\e[0m" "$gbranch" "$a_but_not_c" "$c_but_not_p" "$c_but_m_before_p" "$untracked"
@@ -97,5 +95,5 @@ local Rest='\e[u' # Restore cursor to save point
 
 # ensure that this PS1 and corresponding ANSI Seq's are closed properly
 #PS1='\[\e[0;31m\]♥ \e[0;31m\]\W \[\e[1;33m\]\$\[\e[0m\] '
-PS1='\[\e[1;33;3m\]\w \[\e[0m\]$(tput setaf 2)$(tput bold)$commitstot $commiticon\n $(tput setaf 7)$(tput bold)$(tput setab 4) \[\e[0m\] '
+PS1='\[\e[1;33;3m\]\w \[\e[0m\]$(tput setaf 2)$(tput bold)$commitstot $commiticon\n $(tput setaf 7)$(tput bold)$(tput setab 4)$gitprompt_icon \[\e[0m\] '
 export PS1="\[${Save}\e[${COLUMNS}C\e[${#PS1RHS_stripped}D${PS1RHS}${Rest}\]${PS1}"
