d6a03df0984143e8336de30cfca719197520fbfa
yedhink <yedhin1998@gmail.com>
2018-05-26 18:41:21 +0530

    Fix installation path

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cp -rf -t /home/${USER}/ $DIR/.bashed-gitprompt.sh $DIR/.cal.sh
-cat ./base.sh >> /home/${USER}/.bashrc
+cat $DIR/base.sh >> /home/${USER}/.bashrc
 source /home/${USER}/.bashrc
