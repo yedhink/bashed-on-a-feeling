66fa934f0df295f02434e1138d22a7a94d42e2ab
yedhink <yedhin1998@gmail.com>
2018-05-26 18:05:02 +0530

    26 May 18 : Create/Update new files

--- base.sh
+++ base.sh
@@ -1,8 +1,9 @@
+#!/bin/bash
 # git prompt will be shown only when you move to a git dir
 gitprompt(){
 	git status &> /dev/null
 	if [ "$?" == 0 ];then
-		source /home/${USER}/.bashed-on-a-feeling.sh
+		source /home/${USER}/.bashed-gitprompt.sh
 	else
 		# set this as your prompt for all non-git directories
 		export PS1='\[\e[0;31m\]♥ \e[0;31m\]\W \[\e[1;33m\]\$\[\e[0m\] '
