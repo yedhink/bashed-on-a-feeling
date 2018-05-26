a905d9edbdf222d74b32be2908d74ce746da8261
yedhink <yedhin1998@gmail.com>
2018-05-26 20:33:01 +0530

    Sat 26th May : Fix prompt to working state

--- .bashed-gitprompt.sh
+++ .bashed-gitprompt.sh
@@ -17,9 +17,12 @@
 #  "ahh..ahh..ah..bashed-on-a-feeling!"
 #
 
+# # # # # # # # # # # #
+# set your icons here #
+# # # # # # # # # # # #
 : ${commiticon:=''} # or you can try f737 with a nerd font
 : ${added_but_not_committed:=''}
-: ${committed_but_not_pushed:=''}
+: ${committed_and_clean:=''}
 : ${no_of_files_to_be_pushed:=''}
 : ${committed_but_modified_before_push:=''}
 : ${untracked_files:=''}
@@ -52,7 +55,7 @@ if [ $c_but_not_p -gt 0 ];then
 fi
 
 if [ $c_but_not_p == 0 ];then
-	c_but_not_p="$(tput bold)$(tput setaf 2)$(echo $committed_but_not_pushed)"
+	c_but_not_p="$(tput bold)$(tput setaf 2)$(echo $committed_and_clean)"
 else
 	c_but_not_p="$(tput bold)$(tput setaf 7)$c_but_not_p$(tput bold)$(tput setaf 2)$(echo $no_of_files_to_be_pushed)"
 fi
@@ -73,27 +76,9 @@ printf -v PS1RHS "\e[0m \e[0;1;31m%s %s %s %s %s\e[0m" "$gbranch" "$a_but_not_c"
 
 # Strip ANSI commands before counting length
 PS1RHS_stripped=$(sed "s,\x1B\[[0-9;]*[a-zA-Z],,g" <<<"$PS1RHS")
-
-
 local Save='\e[s' # Save cursor position
 local Rest='\e[u' # Restore cursor to save point
 
-#while read -r Z; do
-#	[[ "$Z" == commit* ]] && cno+=1
-#done <<< "$(/usr/bin/git log 2> /dev/null)"
-#commitstot=$cno
-#commiticon="\\uf737"
-#commiticon=`printf "%b\\n" "$commiticon"`
-
-# Save cursor position, jump to right hand edge, then go left N columns where
-# N is the length of the printable RHS string. Print the RHS string, then
-# return to the saved position and print the LHS prompt.
-
-# Note: "\[" and "\]" are used so that bash can calculate the number of
-# printed characters so that the prompt doesn't do strange things when
-# editing the entered text.
-
-# ensure that this PS1 and corresponding ANSI Seq's are closed properly
-#PS1='\[\e[0;31m\]♥ \e[0;31m\]\W \[\e[1;33m\]\$\[\e[0m\] '
-PS1='\[\e[1;33;3m\]\w \[\e[0m\]$(tput setaf 2)$(tput bold)$commitstot $commiticon\n $(tput setaf 7)$(tput bold)$(tput setab 4)$gitprompt_icon \[\e[0m\] '
+# bashed-git-prompt \m/
+PS1='\[\e[1;33;3m\]\w \[\e[0m\]$(tput setaf 2)$(tput bold)$commitstot $commiticon\n $(tput setaf 7)$(tput bold)$(tput setab 4) $gitprompt_icon\[\e[0m\] '
 export PS1="\[${Save}\e[${COLUMNS}C\e[${#PS1RHS_stripped}D${PS1RHS}${Rest}\]${PS1}"
