1919a2b5a82f70c1c297b8a43a899e04bb250150
yedhink <yedhin1998@gmail.com>
2018-05-27 17:39:41 +0530

    27 May 18 : Create/Update new files

--- .cal.sh
+++ .cal.sh
@@ -12,17 +12,22 @@ while read -r Z; do
 	[[ "$Z" == commit* ]] && ((cno+=1))
 done <<< "$(/usr/bin/git log 2> /dev/null)"
 
-echo -ne "$(git diff --cached --name-only | wc -l)"\
+git rev-list --left-right --count origin/master..."$gbranch" >/dev/null 2>&1
+if [ "$?" == 0 ];then
+	rev=`git rev-list --left-right --count origin/master..."$gbranch"`;
+	beh=$(cut -f1 <<< $rev )
+	ahe=$(cut -f2 <<< $rev )
+else
+	beh="-1"
+	ahe="-1"
+fi
+
+echo -e "$(git diff --cached --name-only | wc -l)"\
 	"$(git diff --stat origin/master.. | wc -l)"\
 	"$(git diff --name-status | wc -l)"\
 	"$(git ls-files --others --exclude-standard | wc -l)"\
 	"$gbranch"\
-	"${cno}" \
+	"${cno}"\
+	"$beh"\
+	"$ahe"
 
-git rev-list --left-right --count origin/master..."$gbranch" >/dev/null 2>&1
-if [ "$?" == 0 ];then
-	rev=`git rev-list --left-right --count origin/master..."$gbranch"`;
-	echo -e "$(cut -f1 <<< $rev )" "$(cut -f2 <<< $rev )"
-else
-	echo -e "-1" "-1"
-fi
