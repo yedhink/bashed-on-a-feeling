991df1e9566bc9e0f158533e499a19e91f206b4c
yedhink <yedhin1998@gmail.com>
2018-05-27 16:58:25 +0530

    27 May 18 : Create/Update new files

--- .bashed-gitprompt.sh
+++ .bashed-gitprompt.sh
@@ -43,15 +43,15 @@ if [ $c_but_not_p -gt 0 ];then
 	((c_but_not_p = c_but_not_p - 1 ))
 fi
 
-if [ $aheadby -eq "0" && $behindby -eq "0" ];then
+if [ $aheadby -eq 0 ] && [ $behindby -eq 0 ];then
 	aheadby="$(tput bold)$(tput setaf 2)$(echo $committed_and_clean)"
 	behindby=""
 else
-	if [ $aheadby != 0 && $behindby != 0 ];then
+	if [ $aheadby != 0 ] && [ $behindby != 0 ];then
 		aheadby="$(tput bold)$(tput setaf 7)$aheadby$(tput bold)$(tput setaf 2)$(echo $ahead)"
 		behindby="$(tput bold)$(tput setaf 7)$behindby$(tput bold)$(tput setaf 2)$(echo $behind)"
 		#set git prompt diverged symbol too
-	elif [ $aheadby == 0 && $behindby != 0 ];then
+	elif [ $aheadby -eq 0 ] && [ $behindby != 0 ];then
 		aheadby=""
 		behindby="$(tput bold)$(tput setaf 7)$behindby$(tput bold)$(tput setaf 2)$(echo $behind)"
 	else
@@ -72,7 +72,7 @@ else
 	untracked="$(tput bold)$(tput setaf 7)$untracked$(tput bold)$(tput setaf 2)$(echo $untracked_files)"
 fi
 # Create a string
-printf -v PS1RHS "\e[0m \e[0;1;31m%s %s %s %s %s\e[0m" "$gbranch" "$a_but_not_c" "$aheadby" "$behindby" "$c_but_m_before_p" "$untracked"
+printf -v PS1RHS "\e[0m \e[0;1;31m%s %s %s %s %s %s\e[0m" "$gbranch" "$a_but_not_c" "$aheadby" "$behindby" "$c_but_m_before_p" "$untracked"
 
 # Strip ANSI commands before counting length
 PS1RHS_stripped=$(sed "s,\x1B\[[0-9;]*[a-zA-Z],,g" <<<"$PS1RHS")
