0dda7c65593a31c4a27c1917261f9b4a10199d1d
yedhink <yedhin1998@gmail.com>
2018-05-28 10:54:04 +0530

    Replace fullpath with ~

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-cp -rf -t /home/${USER}/ $DIR/.bashed-gitprompt.sh $DIR/.cal.sh
-cat $DIR/base.sh >> /home/${USER}/.bashrc
-source /home/${USER}/.bashrc
+cp -rf -t ~ $DIR/.bashed-gitprompt.sh $DIR/.cal.sh
+cat $DIR/base.sh >> ~/.bashrc
+source ~/.bashrc
