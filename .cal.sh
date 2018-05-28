0a130364a65bfeb799cd495278475ce123166464
yedhink <yedhin1998@gmail.com>
2018-05-28 11:55:56 +0530

    28 May 18 : Create/Update new files

--- .cal.sh
+++ .cal.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
 
+git=`whereis git | cut -d " " -f2` 
 while read -ra Z; do
 	if [[ "${Z[@]}" == \*\ * ]]; then
 		gbranch="${Z[1]}"
 		break
 	fi
-done <<< "$(/usr/bin/git branch 2> /dev/null)"
+done <<< "$($git branch 2> /dev/null)"
 
 ((cno=0))
 while read -r Z; do
 	[[ "$Z" == commit* ]] && ((cno+=1))
-done <<< "$(/usr/bin/git log 2> /dev/null)"
+done <<< "$($git log 2> /dev/null)"
 
-git rev-list --left-right --count origin/master..."$gbranch" >/dev/null 2>&1
+$git rev-list --left-right --count origin/master..."$gbranch" >/dev/null 2>&1
 if [ "$?" == 0 ];then
-	rev=`git rev-list --left-right --count origin/master..."$gbranch"`;
+	rev=`$git rev-list --left-right --count origin/master..."$gbranch"`;
 	beh=$(cut -f1 <<< $rev )
 	ahe=$(cut -f2 <<< $rev )
 else
@@ -22,10 +23,10 @@ else
 	ahe="-1"
 fi
 
-echo -e "$(git diff --cached --name-only | wc -l)"\
-	"$(git diff --stat origin/master.. | wc -l)"\
-	"$(git diff --name-status | wc -l)"\
-	"$(git ls-files --others --exclude-standard | wc -l)"\
+echo -e "$($git diff --cached --name-only | wc -l)"\
+	"$($git diff --stat origin/master.. | wc -l)"\
+	"$($git diff --name-status | wc -l)"\
+	"$($git ls-files --others --exclude-standard | wc -l)"\
 	"$gbranch"\
 	"${cno}"\
 	"$beh"\
