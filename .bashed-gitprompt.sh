b3a366eba20a9ccb72e9588a1a6e580f92ff0c6b
yedhink <yedhin1998@gmail.com>
2018-05-28 10:50:57 +0530

    28th May 2018 : Replace ansi completely with tput and add varaibles for colors

--- .bashed-gitprompt.sh
+++ .bashed-gitprompt.sh
@@ -34,42 +34,42 @@
 # # # # # # # # # # # #
 #  the colors for use #
 # # # # # # # # # # # #
-boldGreen="$(tput bold)$(tput setaf 2)"
-boldWhite="$(tput bold)$(tput setaf 7)"
+boldGreen = "$(tput bold)$(tput setaf 2)"
+boldWhite = "$(tput bold)$(tput setaf 7)"
 
 # # # # # # # # # # # #
 #  the colors for PS1 #
 # # # # # # # # # # # #
-ps1Dir="$(tput bold)$(tput setaf 3)"  # Same as - \[\e[1;33;3m\]
-ps1BgBlue="$(tput setab 4)"
-ps1Rst="$(tput sgr0)"  # Reset text - \[\e[0m\]
+ps1Dir    = "$(tput bold)$(tput setaf 3)" # Same as - \[\e[1;33;3m\]
+ps1BgBlue = "$(tput setab 4)"
+ps1Rst    = "$(tput sgr0)"                # Reset text - \[\e[0m\]
 
 read a_but_not_c c_but_not_p c_but_m_before_p untracked gbranch commitstot behindby aheadby<<< $( echo | xargs -n 1 -P 8 ~/.cal.sh )
 
 gbranch="$boldWhite$gbranch"
 
-if [ $a_but_not_c == 0 ];then
+if [ $a_but_not_c == 0 ]; then
 	a_but_not_c=""
 else
 	a_but_not_c="$boldWhite$a_but_not_c$boldGreen$(echo $added_but_not_committed)"
 fi
 
-if [ $c_but_not_p -gt 0 ];then
+if [ $c_but_not_p -gt 0 ]; then
 	((c_but_not_p = c_but_not_p - 1 ))
 fi
 
-if [ $aheadby != -1 ];then
-	if [ $aheadby == 0 ] && [ $behindby == 0 ];then
+if [ $aheadby != -1 ]; then
+	if [ $aheadby == 0 ] && [ $behindby == 0 ]; then
 		gitprompt=$gitprompt_normal
 		aheadby="$boldGreen$(echo $committed_and_clean)"
 		behindby=""
 	else
 		gitprompt=$gitprompt_normal
-		if [ $aheadby != 0 ] && [ $behindby != 0 ];then
+		if [ $aheadby != 0 ] && [ $behindby != 0 ]; then
 			aheadby="$boldWhite$aheadby$boldGreen$(echo $ahead)"
 			behindby="$boldWhite$behindby$boldGreen$(echo $behind)"
 			gitprompt=$gitprompt_diverged
-		elif [ $aheadby == 0 ] && [ $behindby != 0 ];then
+		elif [ $aheadby == 0 ] && [ $behindby != 0 ]; then
 			aheadby=""
 			behindby="$boldWhite$behindby$boldGreen$(echo $behind)"
 		else
@@ -84,17 +84,18 @@ else
 fi
 
 
-if [ $c_but_m_before_p == 0 ];then
+if [ $c_but_m_before_p == 0 ]; then
 	c_but_m_before_p=""
 else
 	c_but_m_before_p="$boldWhite$c_but_m_before_p$boldGreen$(echo $committed_but_modified_before_push)"
 fi
 
-if [ $untracked == 0 ];then
+if [ $untracked == 0 ]; then
 	untracked=""
 else
 	untracked="$boldWhite$untracked$boldGreen$(echo $untracked_files)"
 fi
+
 # Create a string
 printf -v PS1RHS "\e[0m \e[0;1;31m%s %s %s %s %s %s\e[0m" "$gbranch" "$a_but_not_c" "$aheadby" "$behindby" "$c_but_m_before_p" "$untracked"
 
