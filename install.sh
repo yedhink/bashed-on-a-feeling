76448b470c4419d8f3877955df592de5f0a9e7d9
yedhink <yedhin1998@gmail.com>
2018-05-28 20:37:49 +0530

    Fix issue #1 regarding the install script

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-cp -rf -t ~ $DIR/.bashed-gitprompt.sh $DIR/.cal.sh
+cp -rf $DIR/.bashed-gitprompt.sh ~ 
+cp -rf $DIR/.cal.sh ~
 cat $DIR/base.sh >> ~/.bashrc
 source ~/.bashrc
