8478780429c70fe90af6111d68c48b027dfc6a01
yedhink <yedhin1998@gmail.com>
2018-05-30 14:39:08 +0530

    30 May 18 : Create/Update new files

--- install.sh
+++ install.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
+echo "Make sure that you've installed a font, preferably a nerd font, and also the font awesome set before using"
+sleep 1
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-cp -rf $DIR/.bashed-gitprompt.sh ~ 
+cp -rf $DIR/.bashed-gitprompt.sh ~
 cp -rf $DIR/.cal.sh ~
 cat $DIR/base.sh >> ~/.bashrc
 source ~/.bashrc
+echo "Installed!!!"
